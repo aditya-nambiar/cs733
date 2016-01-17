2d085d21b1eefb1f57a4611b949bc23cf4fcaa69
aditya-nambiar <aditya.nambiar007@gmail.com>
2016-01-17 16:02:38 +0530

    Rewrote reading from socket

---
+++ assignment1/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	//"os"
+	"testing"
+)
+
+func TestPrintSomething(t *testing.T) {
+	fmt.Println("Say hi")
+	t.Log("Say bye")
+}
+
+func TestHome(t *testing.T) {
+	t.Logf("..sdfsfsd.")
+
+	conn, _ := net.Dial("tcp", "localhost:8080")
+	//checkError(err)
+	_, _ = conn.Write([]byte("write HEAD\rdsfsdfds\n"))
+
+	_, _ = conn.Write([]byte(" 13 234\r\n"))
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Println(err)
+		fmt.Println(line)
+		if err != nil {
+			conn.Close()
+			break
+
+		}
+	}
+
+}
